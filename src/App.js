e25cfa89b1ba7f2e3ec1829dc3a21d353167533b
sosnierz <37373004+sosnierz@users.noreply.github.com>
2022-03-08 14:20:12 +0100

    Setup projects boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => {
+    return ( 
+       <div>
+           <h2>hello</h2>
+       </div>              
+     );
+}
+ 
+export default App;
