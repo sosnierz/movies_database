33076ffe62e29fe7a9dcfe08f4cf5cf8d266a6d0
sosnierz <37373004+sosnierz@users.noreply.github.com>
2022-03-12 18:45:27 +0100

    Feat: Modal, MovieCard, SearchMovie, delate MovieResult

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
 
+
+import Modal from '../Modal/Modal';
 import block from 'bem-css-modules';
 import { default as MovieCardStyles} from './MovieCard.module.scss'
 
 
-
 const style = block(MovieCardStyles)
 
-const Movie = () => {
-    return ( 
-        <li className={style()}>
-           movie
-        </li>
-     );
+
+
+const MovieCard = ( { handleOnClose, isModalOpen, title, overview, poster_path, release_date,  vote_average}) => {
+  
+  const handleOnCloseModal = e =>{
+    e.preventDefault();
+    handleOnClose();
+}
+
+
+  return (
+  <Modal handleOnClose={handleOnClose} isOpen={isModalOpen} shouldBeCloseOnOutsideClick={true}>
+          <div className={style('content')}>
+          <div className={style('header')}>
+            <h4 className={style('title')}>{title}</h4>
+            <button onClick={handleOnCloseModal} className={style('button')}>X</button>
+          </div>
+          <div className={style('body')}>
+              <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} />
+                <p>About: {overview}</p>
+                <p>Premiere:{release_date}</p>
+                <p>Ranking: {vote_average}</p>
+
+          </div>
+          <div className={style('footer')}>
+            <button className={style('button')}>Add to watchlist</button>
+            <button className={style('button')}>Add to watched</button>
+        </div>
+        </div> 
+
+     
+ </Modal>
+    )
 }
  
-export default Movie;
+export default MovieCard;
--- src/components/SearchMovie/SearchMovie.jsx
+++ src/components/SearchMovie/SearchMovie.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+
+// import MoviesResult from '../MoviesResult/MoviesResult';
+import MovieCard from '../MovieCard/MovieCard';
+
 import block from 'bem-css-modules';
-import { default as SearchMovieStyles} from './SearchMovie.module.scss'
-
-import MoviesResult from '../MoviesResult/MoviesResult';
-
-
+import { default as SearchMovieStyles} from './SearchMovie.module.scss';
 const style = block(SearchMovieStyles)
 
 const SearchMovie = () => {
     const[input, setInput] = useState("");
-    const [results, setResults] = useState([])
+    const [movieData, setMovieData] = useState([])
+    const [results, setResults] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    
 
     const apiURL = "https://api.themoviedb.org/3/search/movie";
 
@@ -30,10 +33,28 @@ const SearchMovie = () => {
           });
         };
     
+        const handleOnClose =() => setIsModalOpen(false);
+       
+       
+        const handleOnClick =(title, overview, poster_path, release_date, vote_average) =>{
+          let data = [title, overview, poster_path, release_date, vote_average]
+          console.warn(data);
+          setMovieData(movie=> [1, ...data])
+          
+          return setIsModalOpen(true);
+      
+        }
+         
+        
         const movies = results.map(movie => (
-            <MoviesResult key={movie.id}
-               {...movie} movie={movie}/>
-        ))
+        <li key={movie.id} movieId={movie.id} {...movie} movie={movie}>
+        <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title}/>
+        <h2>{movie.title}</h2>
+        <button onClick={() => handleOnClick(movie.title, movie.overview, movie.poster_path, movie.release_date,  movie.vote_average)}>More info</button>
+        <button>Add to Watchlist</button>
+        <button>Add to watched</button>      
+        </li>
+     ))
     
     return ( 
         <main className={style()}>
@@ -46,10 +67,11 @@ const SearchMovie = () => {
                 </div>
                 {movies.length>0?<section>
                     <h1>Results: </h1>
-                    {movies}
+                      {movies}
                 </section> : null}
-            </div>   
-      
+            </div> 
+
+            <MovieCard handleOnClose={handleOnClose} isModalOpen={isModalOpen}  title={movieData[1]} overview={movieData[2]} poster_path = {movieData[3]} release_date={movieData[4]} vote_average={movieData[5]} />
         </main>
      );
 }
