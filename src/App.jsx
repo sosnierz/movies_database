9522713ca0809f5b9000957e3635694a8be844f4
sosnierz <37373004+sosnierz@users.noreply.github.com>
2022-03-22 13:07:28 +0100

    add hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Route,  BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route,  HashRouter as Router, Switch } from 'react-router-dom';
 
 import SearchMovie from './components/SearchMovie/SearchMovie';
 import Watched from './components/Watched/Watched';
 import WatchList from './components/WatchList/WatchList';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import './App.scss';
 import MoviesProvider from './Context/MoviesProvider'
 
@@ -27,8 +28,9 @@ const App = () => {
               
               <Route path ='/watchlist'>              
                 <WatchList />
-              </Route>   
+              </Route>  
             </Switch>
+            <Footer />
           </Router>
           </MoviesProvider>
      );
